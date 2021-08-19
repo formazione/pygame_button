84149ac4f4ff5fcd50694bf9262199548866bd26
formazione <gatto.gio@gmail.com>
2021-08-19 21:40:25 +0200

    new commit

--- nice_button_2.py
+++ nice_button_2.py
@@ -1,24 +1,30 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
-
+screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+screen2 = pygame.Surface((600, 400))
+SIZE = pygame.display.Info()
+WIDTH = SIZE.current_w
+HEIGHT = SIZE.current_h
+print(WIDTH, HEIGHT)
 red = (255, 0, 0)
 green = (0, 255, 0)
 
 
 def button(screen, position, text, size=50, color="yellow", bg="darkblue"):
+    """ draws a rectangle and some lines and then blits the text on it """
     font = pygame.font.SysFont("Arial", size)
     text_render = font.render(text, 1, color)
     x, y, w , h = text_render.get_rect()
     x, y = position
     # line(surface, color, (startingpoint), (endingpoint), thickness)
-    pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w + 1, y), 5)
-    pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
-    pygame.draw.rect(screen, bg, (x, y, w + 2, h))
-    return screen.blit(text_render, (x, y))
+    pygame.draw.line(screen2, (150, 150, 150), (x, y), (x + w + 1, y), 5)
+    pygame.draw.line(screen2, (150, 150, 150), (x, y - 2), (x, y + h), 5)
+    pygame.draw.line(screen2, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
+    pygame.draw.line(screen2, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
+    # the rectangle that makes the background
+    pygame.draw.rect(screen2, bg, (x, y, w + 2, h))
+    return screen2.blit(text_render, (x, y))
 
 def start():
     print("Ok, let's go")
@@ -45,6 +51,7 @@ def menu():
                     pygame.quit()
                 elif b2.collidepoint(pygame.mouse.get_pos()):
                     start()
+        screen.blit(pygame.transform.scale(screen2, (WIDTH, HEIGHT)), (0, 0))
         pygame.display.update()
     pygame.quit()
 
