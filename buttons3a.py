85f9050fd6419efbf2921932b623911196a17e16
formazione <gatto.gio@gmail.com>
2021-08-22 08:44:30 +0200

    new commit

--- buttons3a.py
+++ buttons3a.py
@@ -15,14 +15,23 @@ def button(screen, position, text, size, colors="white on blue"):
     pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
     pygame.draw.line(screen, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
     pygame.draw.rect(screen, bg, (x, y, w , h))
-    return screen.blit(text_render, (x, y)) 
+    return screen.blit(text_render, (x, y))
+
+
 
 def start():
     print("Ok, let's go")
 
+
+def hover(button):
+    
+    if button.collidepoint(pygame.mouse.get_pos()):
+        button = button(screen, (300, 300), "Quit me", 50, "red on green")
+    else:
+        button = button(screen, (300, 300), "Quit me", 50, "red on yellow")
+
 def menu():
     """ This is the menu that waits you to click the s key to start """
-    # b0 contains the rect coordinates of the button
     b0 = button(screen, (10, 10), "Here comes the buttons", 55, "red on yellow")
     b1 = button(screen, (300, 300), "Quit me", 50, "red on yellow")
     b2 = button(screen, (500, 300), "Start", 50, "white on green")
@@ -36,8 +45,12 @@ def menu():
                 key_to_start = event.key == pygame.K_s or event.key == pygame.K_RIGHT or event.key == pygame.K_UP
                 if key_to_start:
                     start()
+            if event.type == pygame.MOUSEMOTION:
+                if b1.collidepoint(pygame.mouse.get_pos()):
+                    b1 = button(screen, (300, 300), "Quit me", 50, "red on green")
+                else:
+                    b1 = button(screen, (300, 300), "Quit me", 50, "red on yellow")
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # check when you click if the coordinates of the pointer are in the rectangle of the buttons
                 if b1.collidepoint(pygame.mouse.get_pos()):
                     pygame.quit()
                 elif b2.collidepoint(pygame.mouse.get_pos()):
